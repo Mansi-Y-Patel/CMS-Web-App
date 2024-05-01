dd21c4685ad4dea5e399f2489e46cbb1b39f27f9
Mansi-Y-Patel <mansipatel1743@gmail.com>
2024-05-01 14:03:42 +0530

    changes in about us

--- src/components/aboutus.vue
+++ src/components/aboutus.vue
@@ -13,7 +13,7 @@
                     </div>
                     <div class="grid grid-flow-col">
                         <div class="grid justify-center items-center">
-                            <h2 class="mb-3 text-2xl font-extrabold text-gray-900 dark:text-white">Mansi Patel</h2>
+                            <h2 class="mb-3 text-2xl font-extrabold text-gray-900 dark:text-white">Mansi Y. Patel</h2>
                             <p class="text-black font-bold mb-3">Web Developer | UI/UX designer</p>
                             <p class="text-black font-semibold mb-1">Department: Computer Engineering</p>
                             <p class="text-black font-semibold">Batch: 2020-2024</p>
@@ -30,7 +30,7 @@
                     </div>
                     <div class="grid grid-flow-col">
                         <div class="grid justify-center items-center">
-                            <h2 class="mb-3 text-2xl font-extrabold text-gray-900 dark:text-white">Varun Oza</h2>
+                            <h2 class="mb-3 text-2xl font-extrabold text-gray-900 dark:text-white">Varun N. Oza</h2>
                             <p class="text-black font-bold mb-3">Web Developer</p>
                             <p class="text-black font-semibold mb-1">Department: Computer Engineering</p>
                             <p class="text-black font-semibold">Batch: 2020-2024</p>
@@ -47,13 +47,44 @@
                     </div>
                     <div class="grid grid-flow-col">
                         <div class="grid justify-center items-center">
-                            <h2 class="mb-3 text-2xl font-extrabold text-gray-900 dark:text-white">Varun Oza</h2>
-                            <p class="text-black font-bold mb-3">Web Developer</p>
-                            <p class="text-black font-semibold mb-1">Department: Computer Engineering</p>
-                            <p class="text-black font-semibold">Batch: 2020-2024</p>
+                            <h2 class="mb-3 text-2xl font-extrabold text-gray-900 dark:text-white">Dr. Neha Soni</h2>
+                            <p class="text-black font-bold mb-3">Project Mentor</p>
+                            <p class="text-black font-semibold mb-1">Head of Department of Computer Engineering</p>
                         </div>
                         <div class="text-black items-center">
-                            <a href="https://www.linkedin.com/in/varun-oza/" target="_blank"><i class="fa-brands fa-linkedin text-3xl flex justify-center" style="color:#0077b5;"></i></a>
+                            <a href="https://www.linkedin.com/in/neha-soni-01736016b/" target="_blank"><i class="fa-brands fa-linkedin text-3xl flex justify-center" style="color:#0077b5;"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="p-8 items-center rounded-lg shadow-sm shadow-gray-700">
+                    <div class="mb-6 flex justify-center">
+                            <img class="size-40 rounded-full" src="../images/Milin Patel.jpg">
+                    </div>
+                    <div class="grid grid-flow-col">
+                        <div class="grid justify-center items-center">
+                            <h2 class="mb-3 text-2xl font-extrabold text-gray-900 dark:text-white">Prof. Milin M. Patel</h2>
+                            <p class="text-black font-bold mb-3">Project Guide</p>
+                            <p class="text-black font-semibold mb-1">Faculty of Information Technology</p>
+                        </div>
+                        <div class="text-black items-center">
+                            <a href="https://www.linkedin.com/in/milin-patel-372b52192/" target="_blank"><i class="fa-brands fa-linkedin text-3xl flex justify-center" style="color:#0077b5;"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="p-8 items-center rounded-lg shadow-sm shadow-gray-700">
+                    <div class="mb-6 flex justify-center">
+                            <img class="size-40 rounded-full" src="../images/Keyur Suthar.jpg">
+                    </div>
+                    <div class="grid grid-flow-col">
+                        <div class="grid justify-center items-center">
+                            <h2 class="mb-3 text-2xl font-extrabold text-gray-900 dark:text-white">Prof. Keyur S. Suthar</h2>
+                            <p class="text-black font-bold mb-3">Project Guide</p>
+                            <p class="text-black font-semibold mb-1">Faculty of Computer Engineering</p>
+                        </div>
+                        <div class="text-black items-center">
+                            <a href="https://www.linkedin.com/in/keyur-suthar-a7a2b516/" target="_blank"><i class="fa-brands fa-linkedin text-3xl flex justify-center" style="color:#0077b5;"></i></a>
                         </div>
                     </div>
                 </div>
